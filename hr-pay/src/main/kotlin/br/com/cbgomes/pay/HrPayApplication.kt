a0b3bd7d7483524f0e4c2028ce65e92d83a689e2
Carlos Filho <cbarbosagomesfilho@gmail.com>
2021-01-15 16:17:50 -0300

    Create microservice pay

---
+++ hr-pay/src/main/kotlin/br/com/cbgomes/pay/HrPayApplication.kt
@@ -0,0 +1,11 @@
+package br.com.cbgomes.pay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrPayApplication
+
+fun main(args: Array<String>) {
+    runApplication<HrPayApplication>(*args)
+}
