b903628061c6f76503cd6eee623bee3b46d306c9
Carlos Filho <cbarbosagomesfilho@gmail.com>
2021-01-18 21:31:07 -0300

    updating repository

--- hr-pay/src/main/kotlin/br/com/cbgomes/pay/service/use_case_port/input/PaymentInputService.kt
+++
@@ -1,8 +0,0 @@
-package br.com.cbgomes.pay.service.use_case_port.input
-
-import br.com.cbgomes.pay.web.domain.PaymentOutputPort
-
-interface PaymentInputService {
-
-    fun getPayment(worker: Long, days: Int): PaymentOutputPort
-}
--- hr-pay/src/main/kotlin/br/com/cbgomes/pay/service/use_case_port/output/PaymentOutputService.kt
+++
@@ -1,8 +0,0 @@
-package br.com.cbgomes.pay.service.use_case_port.output
-
-import br.com.cbgomes.pay.web.domain.PaymentOutputPort
-
-interface PaymentOutputService {
-
-    fun getPayment(worker: Long, days: Int): PaymentOutputPort
-}
