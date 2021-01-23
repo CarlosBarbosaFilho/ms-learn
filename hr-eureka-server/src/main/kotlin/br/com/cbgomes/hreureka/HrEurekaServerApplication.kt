b848f5035c3f5db30f9a269c7cbb117427c8d476
Carlos Filho <cbarbosagomesfilho@gmail.com>
2021-01-23 11:56:53 -0300

    udpdated project with eureka server and eureka client

--- hr-eureka-server/src/main/kotlin/br/com/cbgomes/hreureka/HrEurekaServerApplication.kt
+++ hr-eureka-server/src/main/kotlin/br/com/cbgomes/hreureka/HrEurekaServerApplication.kt
@@ -1,13 +1,14 @@
-package br.com.cbgomes.hrworker
+package br.com.cbgomes.hreureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
-@EnableEurekaClient
 @SpringBootApplication
-class HrWorkerApplication
+@EnableEurekaServer
+class HrEurekaServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<HrWorkerApplication>(*args)
+    runApplication<HrEurekaServerApplication>(*args)
 }
--- hr-payment/src/main/kotlin/br/com/cbgomes/HrPaymentApplication.kt
+++ hr-payment/src/main/kotlin/br/com/cbgomes/HrPaymentApplication.kt
@@ -2,9 +2,11 @@ package br.com.cbgomes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableEurekaClient
 @RibbonClient("hr-worker")
 @EnableFeignClients
 @SpringBootApplication
